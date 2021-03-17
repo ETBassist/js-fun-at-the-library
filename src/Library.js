63d41325f8ba4e3e47ead8f325605b9cf8814769
Eugene Theriault <hybridbassist@gmail.com>
2021-03-17 10:50:34 -0700

    Create checkoutBook function in Library.js

--- src/Library.js
+++ src/Library.js
@@ -13,8 +13,20 @@ function addBook(library, book) {
   library.shelves[book.genre].push(book);
 }
 
+function checkoutBook(library, title, genre) {
+  let bookIndex = library.shelves[genre].findIndex((book) => {
+    book.title === title;
+  });
+  let book = library.shelves[genre].splice(bookIndex, 1);
+  if (book.length > 0) {
+    return `You have now checked out ${title} from the ${library.name}`;
+  } else {
+    return `Sorry, there are currently no copies of ${title} available at the ${library.name}`
+  }
+}
+
 module.exports = {
   createLibrary,
   addBook,
-  // checkoutBook
+  checkoutBook
 };
--- test/Library-test.js
+++ test/Library-test.js
@@ -78,7 +78,7 @@ describe("library.js", function() {
   });
   
   describe("checkoutBook", function() {
-    it.skip("should unshelf a book to check out a book to a patron", function() {
+    it("should unshelf a book to check out a book to a patron", function() {
       var dracula = {
         title: "Dracula",
         mainCharacter: { name: "Count Dracula", age: undefined, pronouns: "he/him" },
@@ -109,7 +109,7 @@ describe("library.js", function() {
       assert.equal(result, "You have now checked out Pride and Prejudice from the Denver Public Library")
     });
     
-    it.skip("should only checkout a book if the book is on the shelves", function() {
+    it("should only checkout a book if the book is on the shelves", function() {
       var denverLibrary = createLibrary("Denver Public Library");
           
       var error = checkoutBook(denverLibrary, "The Fifth Season", "fantasy");
