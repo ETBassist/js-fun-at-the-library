2e562fd913ff55dc6160b8189476108aadfdf7ee
Eugene Theriault <hybridbassist@gmail.com>
2021-03-18 12:06:58 -0700

    Create Librarian class and methods

--- src/librarian.js
+++ src/librarian.js
@@ -1,6 +1,35 @@
 class Librarian {
-  constructor() {
-    
+  constructor(name, library) {
+    this.name = name;
+    this.library = library;
+  }
+  
+  greetPatron(patron, morning) {
+    if (morning) {
+      return `Good morning, ${patron}!`;
+    } else {
+      return `Hello, ${patron}!`;
+    }
+  }
+
+  findBook(title) {
+    let hasBook;
+    for (var key in this.library.shelves) {
+      let index = this.library.shelves[key].findIndex((book) => book.title === title);
+      if (index > -1) {
+        hasBook = true;
+        this.library.shelves[key].splice(index, 1);
+      }
+    }
+    if (hasBook === true) {
+      return `Yes, we have ${title}`
+    } else {
+      return `Sorry, we do not have ${title}`
+    }
+  }
+
+  calculateLateFee(numberOfDays) {
+    return Math.ceil(numberOfDays * 0.25);
   }
 }
 
